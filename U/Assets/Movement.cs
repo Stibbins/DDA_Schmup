dc36e44286fc9e3ac3c2131d991df7904eb70da7
Stibbins <john.spjuth@gmail.com>
2015-02-19 14:15:07 +0100

    Unity VS + basic stuff

---
+++ U/Assets/Movement.cs
@@ -0,0 +1,33 @@
+﻿using   UnityEngine;
+using   System.Collections;
+
+public class Movement : MonoBehaviour {
+
+ 
+    public KeyCode Up;
+    public KeyCode Down;
+    public KeyCode Left;
+    public KeyCode Right;
+   
+    
+    [SerializeField]
+    private float _TopSpeed;
+    [SerializeField]
+    private AnimationCurve _Acceleration;
+    [SerializeField]
+    private float _AccelerationFactor;
+    
+
+	
+	void Start () 
+    {
+	
+        
+    }
+	
+	
+	void Update () 
+    {
+	
+    }
+}
