99a807a8d661f2fa78ce862dc9bd46b40390593c
Stibbins <john.spjuth@gmail.com>
2015-02-23 17:41:03 +0100

    Enemy controller and health implemented

---
+++ U/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+    [SerializeField]
+    private float health;
+
+    //TODO: Movement patterns
+    //TODO: Attack patterns
+    //TODO: List tracking thing
+
+	void Start ()  
+    {
+	
+	}
+	
+
+	void Update () {
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+            //TODO: Remove from list
+        }
+	
+	}
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("PlayerShot"))
+        {
+            PlayerDamage playerShot = other.GetComponent<PlayerDamage>();
+            health -= playerShot.damageAmount;
+            playerShot.DestroyInstance();
+        }
+    }
+
+
+
+
+}
+
