52e16e0a01c75aeb10d1f6f9e149cd1093186aab
Stibbins <john.spjuth@gmail.com>
2015-02-23 13:45:26 +0100

    Implemented basic bullet fire and movement

---
+++ U/Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletMovement : MonoBehaviour {
+
+    [SerializeField]
+    private float _bulletSpeed;
+    [SerializeField]
+    private float _bulletLifeTime;
+
+
+    private float _deathTime;
+
+	// Use this for initialization
+	void Start () 
+    {
+        //_bulletSpeed = 0.1f;
+        //_bulletLifeTime = 3f;
+        _deathTime = Time.time + _bulletLifeTime;
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (Time.time < _deathTime)
+        {
+            Vector2 _movement = Vector2.zero;
+            _movement.y += _bulletSpeed;
+            transform.Translate(_movement);
+        }
+        else
+            Destroy(gameObject);
+	}
+}
---
+++ U/Assets/Scripts/Weapons.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapons : MonoBehaviour {
+
+    public KeyCode keyFire;
+
+    [SerializeField]
+    private Transform cannonTransform;
+    [SerializeField]
+    private Object bulletPrefab;
+    [SerializeField]
+    private float fireRate;
+
+
+    private float lastFire;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    if (Input.GetKey(keyFire) && Time.time > lastFire + fireRate)
+        {
+            lastFire = Time.time;
+            Instantiate(bulletPrefab, cannonTransform.position, Quaternion.identity);
+        }
+	}
+}
