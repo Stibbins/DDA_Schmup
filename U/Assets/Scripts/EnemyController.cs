246b9e0210f7f9f3ada5247ce6d7995cc969105d
Stibbins <john.spjuth@gmail.com>
2015-02-25 14:54:17 +0100

    Testing out HOTween

--- U/Assets/Scripts/EnemyController.cs
+++ U/Assets/Scripts/EnemyController.cs
@@ -2,14 +2,17 @@
 using System.Collections;
 using Holoville.HOTween;
 using Holoville.HOTween.Plugins;
+using Holoville.HOTween.Path;
 
 public class EnemyController : MonoBehaviour {
 
     [SerializeField]
     private float health;
-  
+    [SerializeField]
+    private float tweenDuration;
+    private HOPath _hoPath;
 
-    private Transform _playerTransform;
+
 
     //TODO: Movement patterns
     //TODO: Attack patterns
@@ -17,8 +20,12 @@ public class EnemyController : MonoBehaviour {
 
 	void Start ()  
     {
-        _playerTransform = GameObject.Find("Player").transform;
-        HOTween.To(transform, 3, "position", _playerTransform.position);
+
+        _hoPath = GameObject.Find("PathStorage").GetComponent<HOPath>();
+        HOTween.To(transform, tweenDuration, new TweenParms()
+                            .Prop("position", _hoPath.MakePlugVector3Path().Is2D())
+                            .Ease(EaseType.Linear)
+                            );
 	}
 	
 
