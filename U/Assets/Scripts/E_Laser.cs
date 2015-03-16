5edef4946b094a82cea732570573fa1aabf255ff
Stibbins <john.spjuth@gmail.com>
2015-03-16 21:12:50 +0100

    more lasors

--- U/Assets/Scripts/E_Laser.cs
+++ U/Assets/Scripts/E_Laser.cs
@@ -12,6 +12,7 @@ public class E_Laser : MonoBehaviour {
     private float _attackDamage;
     private Vector2 _playerPosition;
     private float _attackTime;
+    private float _attackLength = 1;
     
 
 
@@ -29,16 +30,29 @@ public class E_Laser : MonoBehaviour {
         Vector2 _2dPosition = new Vector2();
         _2dPosition.x = transform.position.x;
         _2dPosition.y = transform.position.y;
-        
-        
-        transform.LookAt(_playerPosition);
-        RaycastHit2D _rayHit = Physics2D.Raycast(transform.position, Vector2.up);
-        if (_rayHit != null && _rayHit.transform.CompareTag("Player"))
+
+        if (_lockRotation == false)
         {
-            _lockRotation = false;
-            _attackTime = Time.time + _attackDelay;
+            // TODO: Build another rotation function
+            transform.LookAt(_playerPosition);
+            RaycastHit2D _rayHit = Physics2D.Raycast(transform.position, Vector2.up);
+            if (_rayHit != null && _rayHit.transform.CompareTag("Player"))
+            {
+                _lockRotation = true;
+                _attackTime = Time.time + _attackDelay;
+            }
         }
+        
+        
 	    
+        if (_lockRotation == true && Time.time > _attackTime && Time.time < _attackTime + _attackLength)
+        {
+            //Display large laser
+            //Own prefab?
+            Debug.Log("LazoR!");
+        }
+
+
 
 	}
 }
