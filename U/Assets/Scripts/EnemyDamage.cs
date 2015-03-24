b52ef797a329bb853b76068cee57a41c793b083b
Stibbins <john.spjuth@gmail.com>
2015-03-24 16:29:41 +0100

    Player health

--- U/Assets/Scripts/EnemyDamage.cs
+++ U/Assets/Scripts/EnemyDamage.cs
@@ -6,13 +6,4 @@ public class EnemyDamage : MonoBehaviour {
 
     public float damageAmount;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
---
+++ U/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    //Singleton
+    private static PlayerController _instance;
+    public static PlayerController instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<PlayerController>();
+            }
+            return _instance;
+        }
+    }
+
+
+    public float playerMaxHealth;
+    public float _currentHealth
+    {
+        get
+        {
+            return _currentHealth;
+        }
+        private set
+        {
+            _currentHealth = value;
+        }
+    }
+
+
+    //---------------
+
+	void Awake () 
+    {
+	
+	}
+
+	void Update () 
+    {
+	
+	}
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if (other.CompareTag("EnemyShot"))
+        {
+            EnemyDamage damageClass = other.GetComponent<EnemyDamage>();
+            _currentHealth -= damageClass.damageAmount;
+            Destroy(other.gameObject);
+            Debug.Log("Current health: " + _currentHealth);
+        }
+    }
+}
