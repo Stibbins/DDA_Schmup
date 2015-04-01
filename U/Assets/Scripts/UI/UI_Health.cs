ec39cf8d8e00cd23b48e95116796d1da83f9d23b
Stibbins <john.spjuth@gmail.com>
2015-04-01 10:38:43 +0200

    Health UI

---
+++ U/Assets/Scripts/UI/UI_Health.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UI_Health : MonoBehaviour {
+
+    private Text _textComponent;
+    private PlayerController _playerController;
+    void Awake()
+    {
+        _textComponent = GetComponent<Text>();
+        _playerController = PlayerController.instance;
+    }
+
+
+    void Update()
+    {
+        _textComponent.text = "Current health: " + _playerController.currentHealth;
+    }
+}
